1f79b9cc6e931324b739cf5ae0af386b795fe5a1
Zaslow <dcz4@clg62-m128.labs.pitt.edu>
2015-04-10 17:01:48 -0400

    Initial Commit

---
+++ DCZSimpleWebBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DCZSimpleWebBrowser
+//
+//  Created by Zaslow, Dean Clayton on 4/10/15.
+//  Copyright (c) 2015 Zaslow, Dean Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
